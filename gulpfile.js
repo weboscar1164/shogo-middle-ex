996b57ac9fcebda5282da74554e6aa8422a1a40a
webshoposcar1164 <cheaptrip1164@gmail.com>
2023-03-14 06:35:48 +0900

    gulpインストール

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const { src, dest, watch, series, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssdeclsort = require("css-declaration-sorter");
+const gcmq = require("gulp-group-css-media-queries");
+const mode = require("gulp-mode")();
+const browserSync = require("browser-sync");
+
+const compileSass = (done) => {
+	const postcssPlugins = [
+		autoprefixer({
+			grid: "autoplace",
+			cascade: false,
+		}),
+		cssdeclsort({ order: "alphabetical" }),
+	];
+	src("./src/scss/**/*.scss", { sourcemaps: true })
+		.pipe(
+			plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
+		)
+		.pipe(sass({ outputStyle: "expanded" }))
+		.pipe(postcss(postcssPlugins))
+		.pipe(mode.production(gcmq()))
+		.pipe(dest("./dist/css", { sourcemaps: "./sourcemaps" }));
+	done();
+};
+
+const buildServer = (done) => {
+	browserSync.init({
+		port: 8080,
+		// 静的サイト
+		server: { baseDir: "./" },
+		// 動的サイト
+		// files: ["./**/*.php"],
+		// proxy: "http://localsite.local/",
+		open: true,
+		watchOptions: {
+			debounceDelay: 1000,
+		},
+	});
+	done();
+};
+
+const browserReload = (done) => {
+	browserSync.reload();
+	done();
+};
+
+const watchFiles = () => {
+	watch("./src/scss/**/*.scss", series(compileSass, browserReload));
+};
+
+module.exports = {
+	sass: compileSass,
+	default: parallel(buildServer, watchFiles),
+};
